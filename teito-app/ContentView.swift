f221dfbb70fb9e02203eb34f40c35dd8b360a866
seo matsushita <seo.matsushita@kana-me.com>
2022-10-13 19:39:29 +0900

    first commit

---
+++ teito-app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  teito-app
+//
+//  Created by 松下世緒 on 2022/10/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ teito-app/teito_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  teito_appApp.swift
+//  teito-app
+//
+//  Created by 松下世緒 on 2022/10/13.
+//
+
+import SwiftUI
+
+@main
+struct teito_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
